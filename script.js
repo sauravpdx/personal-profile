eb41323683b5bd9c65062f8505404bbcbd19644b
sauravkumar329 <46621822+sauravkumar329@users.noreply.github.com>
2023-03-15 16:31:50 -0700

    Button & section colors and scheduling added

---
+++ script.js
@@ -0,0 +1,5 @@
+const schedulingButton = document.querySelector("#scheduling-button");
+
+schedulingButton.addEventListener("click", () => {
+  window.open("https://calendly.com/saurav2/30min", "_blank");
+});
